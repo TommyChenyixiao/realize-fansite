2cc158aded630d0e5b026a911b8b84eabe0e125f
YixiaoChen <yixiaotommychen@outlook.com>
2026-08-24 23:43:31 -0700

    字体改按偶像官网惯例:中文干净黑体+西文Comfortaa;微博图标改为跟随文字的紫色剪影

--- js/app.js
+++ js/app.js
@@ -46,7 +46,7 @@
     if (g.agency) meta.push("运营:" + g.agency);
     if (g.manager) meta.push("经纪人:" + g.manager);
     setText("group-sub", meta.join(" · "));
-    const wbIcon = '<img class="wb-icon" src="assets/weibo.png" alt="">';
+    const wbIcon = '<span class="wb-icon"></span>';
     const groupLinks = [];
     if (g.weibo) groupLinks.push('<a href="' + esc(g.weibo) + '" target="_blank" rel="noopener">' + wbIcon + "官方微博</a>");
     if (g.managerWeibo) {
@@ -115,7 +115,7 @@
           .map((url) =>
             '<a href="' + esc(url) + '" target="_blank" rel="noopener">' +
             (url.includes("weibo")
-              ? '<img class="wb-icon" src="assets/weibo.png" alt="">微博'
+              ? '<span class="wb-icon"></span>微博'
               : esc(url.replace(/^https?:\/\//, "").split("/")[0])) +
             "</a>")
           .join(" ");
@@ -186,7 +186,7 @@
     const links = (m.socials || [])
       .map((url) =>
         '<a class="modal-weibo" href="' + esc(url) + '" target="_blank" rel="noopener">' +
-        '<img class="wb-icon wb-icon-light" src="assets/weibo.png" alt="">她的微博</a>')
+        '<span class="wb-icon"></span>她的微博</a>')
       .join(" ");
     document.getElementById("modal-body").innerHTML =
       photo +
